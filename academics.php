4a04983d7028288e319d14ab4d25283209804a6d
djmp_09 <djmp_09@yahoo.com>
2018-03-16 02:41:22 +0800

    Added first module

--- academics.php
+++ academics.php
@@ -2,6 +2,17 @@
 	include("connect.php");
 	include("template.html");
 	include("functions.php");
+
+	if(isset($_POST['submit'])){
+		$subjcode = $_POST['subjcode'];
+		$num = $_POST['num'];
+		$url = $_POST['url'];
+		$sql = "INSERT INTO subject_prof(subject_code, num_of_prof) VALUES ('$subjcode', '$num')";
+		$query = mysqli_query($con, $sql);
+		if($query){
+			header("Location: ".$url);
+		}
+	}
 ?>
 <!DOCTYPE html>
 <html>
@@ -45,33 +56,39 @@
 	<div id="subj_table">
 		<?php
 			if(isset($_GET['major'])){
-			echo "
-				<table border='1'>
-					<tr>
-						<th>Subject Code</th>
-						<th>Subject Name</th>
-						<th>Number</th>
-						<th>Action</th>
-					</tr>
-			";
-			$get_course = $_GET['course'];
-			$get_major = $_GET['major'];
-			$table = clean_course($get_course)."_".clean_major($get_major);
-			$sql = "SELECT subjectcode, subjectname FROM `$table`";
-			$query = mysqli_query($con, $sql);
-			if(mysqli_num_rows($query)>0){
-				while($row = mysqli_fetch_assoc($query)){
-					echo "<form method='POST' action='academics.php'>";
-					echo "<tr>";
-					echo "<td>".$row['subjectcode']."</td>";
-					echo "<td>".$row['subjectname']."</td>";
-					echo "<td><input type='number'></td>";
-					echo "<input type='hidden' name='subjcode' value='".$row['subjectcode']."'>";
-					echo "<td><input type='submit' name='submit' value='submit'></td>";
-					echo "</tr></form>";
+				echo "
+					<table border='1'>
+						<tr>
+							<th>Subject Code</th>
+							<th>Subject Name</th>
+							<th>Number</th>
+							<th>Action</th>
+						</tr>
+				";
+				$get_course = $_GET['course'];
+				$get_major = $_GET['major'];
+				$table = clean_course($get_course)."_".clean_major($get_major);
+				$sql = "SELECT $table.subjectcode, $table.subjectname FROM `$table` LEFT JOIN `subject_prof` ON $table.subjectcode = subject_prof.subject_code WHERE subject_prof.subject_code IS NULL";
+				$query = mysqli_query($con, $sql);
+				// "SELECT participants.event_id, participants.student_number, participants.first_name, participants.middle_name, participants.last_name, participants.section FROM participants LEFT JOIN attendance ON attendance.event_id = participants.event_id WHERE attendance.event_id IS NULL"
+				if($query){
+					if(mysqli_num_rows($query)>0){
+						while($row = mysqli_fetch_assoc($query)){
+							echo "<form method='POST' action='academics.php'>";
+							echo "<tr>";
+							echo "<td>".$row['subjectcode']."</td>";
+							echo "<td>".$row['subjectname']."</td>";
+							echo "<td><input type='number' name='num' required='true'></td>";
+							echo "<input type='hidden' name='subjcode' value='".$row['subjectcode']."'>";
+							echo "<input type='hidden' name='url' value='".$_SERVER['REQUEST_URI']."'>";
+							echo "<td><input type='submit' name='submit' value='submit'></td>";
+							echo "</tr></form>";
+						}
+					}
+					echo "</table>";
+				} else {
+					echo "<tr><td colspan='4' align='center'>EMPTY</td></tr>";
 				}
-			}
-			echo "</table>";
 			}
 		?>
 	</div>
--- index.php
+++ index.php
@@ -1,5 +1,61 @@
 <?php
+	include("connect.php");
 
-	include("template.html");
+	if(isset($_POST['submit'])){
+		$user = $_POST['user'];
+		$pass = $_POST['pass'];
+		$sql = "SELECT idnum FROM admin WHERE idnum = '$user' AND password = '$pass'";
+		$query = mysqli_query($con, $sql);
+		if(mysqli_num_rows($query)>0){
+			header("Location: academics.php");
+		}
+	}
 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LOGIN</title>
+	<link rel="stylesheet" type="text/css" href="navbar.css">
+	<style>
+		#login {
+			margin: auto;
+		    width: 50%;
+		    padding: 10px;
+		    margin-top: 50px;
+		    margin-left: 40%;
+		}
+	</style>
+</head>
+<body>
+	<div id="wrapper">    
+	    <ul>
+	        <label id="tag">CAP COLLEGE FOUNDATION, INC.</label>
+	        <a href="index.php"><img src="logo.png" id="logo"></a>
+	        <img src="logout.png" id="logout">
+	    </ul>
+	</div>
+	<div id="login">
+		<form method="POST" action="index.php">
+		<table border="1">
+			<tr>
+				<th colspan="2">LOGIN</th>	
+			</tr>
+			<tr>
+				<td>Username:</td>
+				<td><input type="text" name="user"></td>
+			</tr>
+			<tr>
+				<td>Password:</td>
+				<td><input type="password" name="pass"></td>
+			</tr>
+			<tr>
+				<td colspan="2">
+					<input type="submit" name="submit" value="LOGIN" style="width:100%;">
+				</td>
+			</tr>
+		</table>
+		</form>
+	</div>
+</body>
+</html>
